c463348ce445a0136e8fe473ec8eb9f0afc6aba6
ParkSnoopy <117149837+ParkSnoopy@users.noreply.github.com>
2025-01-27 04:40:22 +0900

    add: installable binary
    add 'bin/main.rs' to make `which-rs` installable via `cargo`

---
+++ src/bin/main.rs
@@ -0,0 +1,25 @@
+use which::which;
+use std::env;
+
+fn main() {
+	if let Some(targets) = {
+		let args: Vec<String> = env::args().collect();
+		if args.len() >= 2 {
+			Some(args[1..].to_owned().clone())
+		} else {
+			None
+		}
+	} {
+		for target in targets {
+			match which(&target) {
+				Ok(result) => match result.into_os_string().into_string() {
+					Ok(string) => println!("{}", string),
+					Err(osstring) => println!("{:?}", osstring),
+				},
+				Err(_) => println!("(command '{target}' not found)"),
+			}
+		}
+	} else {
+		println!();
+	};
+}
